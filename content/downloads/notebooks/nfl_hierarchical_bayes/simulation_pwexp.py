31058dbaa04f49ac6a96596051c798c326aeafe5
DanielWeitzenfeld <dweitzenfeld@gmail.com>
2015-01-28 10:18:49 -0600

    nfl_hierarchical_bayes

---
+++ content/downloads/notebooks/nfl_hierarchical_bayes/simulation_pwexp.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+import statsmodels.api as sm
+from patsy import dmatrices
+
+
+FIELD_GOAL_RANGE = 66
+MINUTES_PER_YARD = 0
+MINUTES_INTERCEPT = 0
+PUNT_DISTANCE = 40
+LOSING_BADLY_THRESHOLD = 17
