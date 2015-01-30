40241189569410b405835f4a696f1418027267f0
DanielWeitzenfeld <dweitzenfeld@gmail.com>
2015-01-30 10:02:46 -0600

    more nfl

--- content/downloads/notebooks/nfl_hierarchical_bayes/__init__.py
+++ content/downloads/notebooks/nfl_hierarchical_bayes/__init__.py
@@ -1,1 +1,24 @@
 
+
+#FIELD_GOAL_RANGE = 66 # In simulations, drives dying before this point will punt (regardless of game situation).
+#PIECES = [0, 15, 40, 66, 99] # Divide the field up into chunks for piecewise exponential modeling.
+#REDZONE_PIECE = 3
+#PIECES = [0, 13, 66, 99]
+#REDZONE_PIECE = 2
+
+FIELD_GOAL_RANGE = 67
+PIECES = [0, 13, 75, 99]
+REDZONE_PIECE = 2
+
+PUNT_DISTANCE = 38 # net of return
+PUNT_CLOCK_TIME = 15./60
+TOUCHDOWN_CLOCK_TIME = 15./60
+FG_CLOCK_TIME = 15./60
+LOSING_BADLY_THRESHOLD = 16
+SIMULATION_TIE_BREAKER_COIN_FLIP_HOME_ADVANTAGE = .03
+
+
+def piece_lengths(PIECES):
+    return [PIECES[i + 1] - PIECES[i] for i in range(len(PIECES) - 1)]
+
+PIECE_LENGTHS = piece_lengths(PIECES)
--- content/downloads/notebooks/nfl_hierarchical_bayes/elapsed_time.py
+++ content/downloads/notebooks/nfl_hierarchical_bayes/elapsed_time.py
@@ -1,1 +1,30 @@
+import statsmodels.api as sm
+from patsy import dmatrices
 
+# Simple linear model to estimate elapsed time of a drive, as a function of drive length in yards.
+#
+
+MINUTES_PER_YARD = 0
+MINUTES_INTERCEPT = 0
+READY_TO_USE = False
+
+
+def set_elapsed_time_per_yard(df_drive):
+    y, X = dmatrices('elapsed_clock ~ yards_end_minus_start_zero_plus', data=df_drive, return_type='dataframe')
+    model = sm.OLS(y, X)
+    results = model.fit()
+    global MINUTES_PER_YARD
+    MINUTES_PER_YARD = results.params['yards_end_minus_start_zero_plus']
+    global MINUTES_INTERCEPT
+    MINUTES_INTERCEPT = results.params['Intercept']
+    global READY_TO_USE
+    READY_TO_USE = True
+    return
+
+
+def drive_time_elapsed(drive_length):
+    if READY_TO_USE:
+        return MINUTES_INTERCEPT + MINUTES_PER_YARD * drive_length
+    else:
+        print 'Run set_elapsed_time_per_yard() first to fit model.'
+        raise Exception
